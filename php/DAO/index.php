8488880e7bdfe70cb0106e2c1b1967733d512243
MarcosPaulo <root@vmdebian9>
2018-08-15 10:58:41 -0300

    dao v.2

--- php/DAO/index.php
+++ php/DAO/index.php
@@ -8,7 +8,28 @@ $usuarios = $sql-> select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 */
+
+/* Carrega um usuário
 $root = new Usuario();
 $root->loadbyId(3);
 echo $root;
+*/
+
+/*
+// Carrega vários usuários
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+/*
+// Carrega uma lista de usuários buscando pelo login
+$search = Usuario::search("jo");
+echo json_encode($search);
+*/
+
+// Carrega um usuario, usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("jose", "123456");
+echo $usuario;
 ?>
